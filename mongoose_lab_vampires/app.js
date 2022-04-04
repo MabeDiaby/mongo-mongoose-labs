e3082c4dcf92db9f84adfc5d4fe96d07bebc2a0b
Mabe-Diaby <diabymabe@gmail.com>
2022-04-04 19:34:16 -0400

    mongo

--- mongoose_lab_vampires/app.js
+++ mongoose_lab_vampires/app.js
@@ -5,7 +5,7 @@ Set up and Configuration
 const mongoose = require("mongoose");
 const seedData = require("./models/seed_vampires.js");
 const Vampire = require("./models/vampire.js");
-
+// console.log(seedData);
 // Configuration
 const mongoURI = "mongodb://localhost:27017/vampires";
 const db = mongoose.connection;
@@ -25,3 +25,68 @@ db.on("open", () => {
 /**********************************************************************
 Write Your Code Below
 **********************************************************************/
+// Vampire.insertMany(seedData, (err, vampires) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log('added provided vampire data', vampires);
+//   mongoose.connection.close();
+// });
+
+// Vampire.insertMany(seedData, 
+//   [{
+//     name: "Chocula Peter",
+//     dob: new Date(1801, 3, 17, 14, 43),
+//     eye_color: "green",
+//     loves: ["books", "blood", "wine"],
+//     location: "Paris, France",
+//     gender: "f",
+//     victims: 6030
+//   },
+//   {
+//     name: "Damien Standbury",
+//     dob: new Date(1906, 2, 18, 50, 3),
+//     hair_color: "brown",
+//     eye_color: "hazel",
+//     loves: ["vodka", "fancy clothes", "dogs"],
+//     location: "Salem, Massachusetts",
+//     gender: "m",
+//     victims: 36
+//   },
+//   {
+//     name: "Selena Gomez",
+//     dob: new Date(1605, 3, 13, 11, 3),
+//     hair_color: "blue",
+//     eye_color: "green",
+//     loves: ["travel", "hats", "shopping"],
+//     location: "Manhattan, New York",
+//     gender: "f",
+//     victims: 524
+//   },
+//   {
+//     name: "James Bond",
+//     dob: new Date(1007, 10, 3, 15, 4),
+//     hair_color: "light brown",
+//     eye_color: "green",
+//     loves: ["drinking", "food", "suits", "fighting"],
+//     location: "Manhattan, New York",
+//     gender: "m",
+//     victims: 524
+//   }],
+//   (err, vampires) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log('added provided vampire data', vampires);
+//   mongoose.connection.close();
+// });
+
+// Vampire.insertMany(seedData, 
+  
+//   (err, vampires) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log('added provided vampire data', vampires);
+//   mongoose.connection.close();
+// });
